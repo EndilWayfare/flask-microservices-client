ae73560671dc925e6b1e8efbdc512d92b4efbb2d
rudasoftware <software@ruda.com>
2017-08-16 15:53:06 -0700

    Added registration and login forms

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 import About from './About';
 import AddUser from './components/AddUser';
+import Form from './components/Form';
+import NavBar from './components/NavBar';
 import UsersList from './components/UsersList';
 
 
@@ -13,11 +15,19 @@ class App extends Component {
         this.state = {
             users: [],
             username: '',
-            email: ''
+            email: '',
+            title: 'TestDriven.io',
+            formData: {
+                username: '',
+                email: '',
+                password: ''
+            }
         }
 
         this.addUser = this.addUser.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleUserFormChange = this.handleUserFormChange.bind(this);
+        this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
     }
 
     componentDidMount() {
@@ -50,29 +60,89 @@ class App extends Component {
         this.setState(obj);
     }
 
+    handleUserFormChange(event) {
+        // 'event' argument is a React SyntheticEvent, so it will be invalidated
+        // when this handler function exits. setState is called asynchronously,
+        // so 'event' will already be reclaimed. Cache the target name and value
+        // for later use.
+        const targetName = event.target.name;
+        const targetValue = event.target.value;
+        this.setState(
+            (prevState, props) => {
+                const obj = {formData: {...prevState.formData}};
+                obj.formData[targetName] = targetValue;
+                return obj;
+            });
+    }
+
+    handleUserFormSubmit(event) {
+        event.preventDefault();
+        const formType = window.location.href.split('/').pop();
+        let data;
+        if (formType === 'login') {
+            data = {
+                email: this.state.formData.email,
+                password: this.state.formData.password
+            }
+        }
+        else if (formType === 'register') {
+            data = {
+                username: this.state.formData.username,
+                email: this.state.formData.email,
+                password: this.state.formData.password
+            }
+        }
+
+        const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`
+        axios.post(url, data)
+        .then((res) => {
+            console.log(res.data);
+        })
+        .catch((err) => { console.log(err); });
+    }
+
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6">
-                        <br/>
-                        <Switch>
-                            <Route exact path='/' render={() => (
-                                <div>
-                                    <h1>All Users</h1>
-                                    <hr/><br/>
-                                    <AddUser
-                                        username={this.state.username}
-                                        email={this.state.email}
-                                        addUser={this.addUser}
-                                        handleChange={this.handleChange}
+            <div>
+                <NavBar title={this.state.title} />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <br/>
+                            <Switch>
+                                <Route exact path='/' render={() => (
+                                    <div>
+                                        <h1>All Users</h1>
+                                        <hr/><br/>
+                                        <AddUser
+                                            username={this.state.username}
+                                            email={this.state.email}
+                                            addUser={this.addUser}
+                                            handleChange={this.handleChange}
+                                        />
+                                        <br />
+                                        <UsersList users={ this.state.users } />
+                                    </div>
+                                )} />
+                                <Route exact path='/about' component={About} />
+                                <Route exact path='/register' render={() => (
+                                    <Form
+                                        formType={'Register'}
+                                        formData={this.state.formData}
+                                        handleFormChange={this.handleUserFormChange}
+                                        handleFormSubmit={this.handleUserFormSubmit}
                                     />
-                                    <br />
-                                    <UsersList users={ this.state.users } />
-                                </div>
-                            )} />
-                            <Route exact path='/about' component={About} />
-                        </Switch>
+                                )} />
+                                <Route exact path='/login' render={() => (
+                                    <Form
+                                        formType={'Login'}
+                                        formData={this.state.formData}
+                                        handleFormChange={this.handleUserFormChange}
+                                        handleFormSubmit={this.handleUserFormSubmit}
+                                    />
+                                )}/>
+                            </Switch>
+                        </div>
                     </div>
                 </div>
             </div>
