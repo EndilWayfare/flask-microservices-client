fee1cf7fd2528f70ceaeb67371b914185bc69dac
rudasoftware <software@ruda.com>
2017-08-16 13:21:50 -0700

    Added client-side routing with react-router
    Added test About route at '/about'

---
+++ src/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const About = () => (
+    <div>
+        <h1>About</h1>
+        <hr /><br />
+        <p>
+            Add something relevant here.
+            Otherwise, everyone will realize you are a hack.
+        </p>
+    </div>
+)
+
+export default About;
